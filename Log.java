0dd7fb749550e7111376fab654e881cad404d7e2
Ricardo Leitão <rleitao@lqd.io>
2014-10-18 21:29:40 +0100

    Add Log parser
    Signed-off-by: Ricardo Leitão <rleitao@lqd.io>

---
+++ Log.java
@@ -0,0 +1,45 @@
+
+public class Log {
+
+    private String [] mLog;
+
+    public Log(String raw) {
+        mLog = raw.split(",");
+        sanitizeAttributes();
+    }
+
+    public String getTowerId() {
+        return mLog[0];
+    }
+
+    public String getDate() {
+        return mLog[1];
+    }
+
+    public String getTime() {
+        return mLog[2];
+    }
+
+    public String getBirdId() {
+        return mLog[3];
+    }
+
+    public String getWeight() {
+        return mLog[4];
+    }
+
+    public String getWingSpan() {
+        return mLog[5];
+    }
+
+    public String getWeather() {
+        return mLog[6];
+    }
+
+    private void sanitizeAttributes() {
+        for(String s : mLog) {
+            s = s.trim();
+        }
+    }
+
+}
