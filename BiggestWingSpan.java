c057c52bffbe54824f9b7cedad190a9d7ee4bcf6
diogorosa <diogo.m.c.rosa@gmail.com>
2014-10-20 14:10:13 +0100

    Improvements in mapper function
    Mapper only gathers info that happened in raining conditions
    
    Signed-off-by: diogorosa <diogo.m.c.rosa@gmail.com>

--- BiggestWingSpan.java
+++ BiggestWingSpan.java
@@ -13,50 +13,58 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class BiggestWingSpan {
 
-  public static class WingSpanMapper extends Mapper<Object, Text, Text, IntWritable>{
+    public static class WingSpanMapper extends
+            Mapper<Object, Text, Text, IntWritable> {
 
-    private IntWritable wingspan = new IntWritable();
-    private Text mapkey = new Text();
-    private Log log;
+        private IntWritable wingspan = new IntWritable();
+        private Text mapkey = new Text();
+        private String weatherCondition = "3";
+        private Log log;
 
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        log = new Log(itr.nextToken());
-        mapkey.set(log.getTowerId() + "," + log.getDate() + "," + log.getWeather());
-        wingspan.set(Integer.valueOf(log.getWingSpan()));
-        context.write(mapkey, wingspan);
-      }
-    }
-
-  }
-
-  public static class WingSpanReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
-
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-      int max = 0;
-      for (IntWritable val : values) {
-        if(val.get() > max) {
-          max = val.get();
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                log = new Log(itr.nextToken());
+                if (log.getWeather().equals(weatherCondition)) {
+                    mapkey.set(log.getTowerId() + "," + log.getDate() + ","
+                            + log.getWeather());
+                    wingspan.set(Integer.valueOf(log.getWingSpan()));
+                    context.write(mapkey, wingspan);
+                }
+            }
         }
-      }
-      result.set(max);
-      context.write(key, result);
-    }
-  }
 
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "BiggestWingSpan");
-    job.setJarByClass(BiggestWingSpan.class);
-    job.setMapperClass(WingSpanMapper.class);
-    job.setCombinerClass(WingSpanReducer.class);
-    job.setReducerClass(WingSpanReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+    }
+
+    public static class WingSpanReducer extends
+            Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                Context context) throws IOException, InterruptedException {
+            int max = 0;
+            for (IntWritable val : values) {
+                if (val.get() > max) {
+                    max = val.get();
+                }
+            }
+            result.set(max);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "BiggestWingSpan");
+        job.setJarByClass(BiggestWingSpan.class);
+        job.setMapperClass(WingSpanMapper.class);
+        job.setCombinerClass(WingSpanReducer.class);
+        job.setReducerClass(WingSpanReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
